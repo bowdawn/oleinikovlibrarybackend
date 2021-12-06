725e3afe8fe06d9e24fb7617946f80cfb74e915a
Oleinikov, Bogdan Konstantinovich <boleinikov3@gatech.edu>
2021-12-05 23:16:40 -0500

    Remove node_modules folder

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import express, { json } from "express";
-const app = express();
+import mongoose from 'mongoose'
 import product from "./api/product.js";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+
+
+const PORT = process.env.PORT || 5000;
+
+const DB_URL = process.env.DB_URL || "Error - DB URL is not definted";
+
+const app = express();
 
 app.use(json({ extended: false }));
 
@@ -8,12 +19,22 @@ app.use("/api/product", product);
 
 app.use("/", ( req, res) => {
     try {
-      res.json("Oleinikov Library");
+      res.status(200).json("Oleinikov Library");
     } catch (error) {
       console.error(error);
       return res.status(500).send("Server error");
     }
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL);
+    app.listen(PORT, () => app.listen(PORT, () => console.log(`Server is running in port ${PORT}` ) ))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+startApp()
+
+
