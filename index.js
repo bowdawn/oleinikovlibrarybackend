c32849e534ab710e8e0ba395ced7fa376ee89ef9
Oleinikov, Bogdan Konstantinovich <boleinikov3@gatech.edu>
2021-12-05 22:14:27 -0500

    express init

---
+++ index.js
@@ -0,0 +1,10 @@
+import express, { json } from "express";
+const app = express();
+import product from "./api/product.js";
+
+app.use(json({ extended: false }));
+
+app.use("/api/product", product);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
