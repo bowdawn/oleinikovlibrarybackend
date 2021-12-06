f9cb7fcaf26f244026ca855e673ed6c2451dcdcb
Oleinikov, Bogdan Konstantinovich <boleinikov3@gatech.edu>
2021-12-05 22:24:28 -0500

    default route

--- index.js
+++ index.js
@@ -6,5 +6,14 @@ app.use(json({ extended: false }));
 
 app.use("/api/product", product);
 
+app.use("/", ( req, res) => {
+    try {
+      res.json("Oleinikov Library");
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send("Server error");
+    }
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
