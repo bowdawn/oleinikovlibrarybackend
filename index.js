55d2b30e9ba7c62ad4ae2b9f0cd13fb010d4c612
Oleinikov, Bogdan Konstantinovich <boleinikov3@gatech.edu>
2021-12-27 13:59:09 -0500

    cors update

--- index.js
+++ index.js
@@ -17,19 +17,7 @@ var allowedOrigins = ['http://localhost:3000',
 
 const app = express();
 
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors({}));
 
 app.use(express.json());
 app.use(fileUpload({}))
