c76b03122a98b470e1a79cef279d85bcbe6574c7
Bogdan Oleinikov <boleinikov@gmail.com>
2024-10-09 22:47:54 -0400

    edit

--- index.js
+++ index.js
@@ -16,8 +16,24 @@ import path from 'path';
 
 const app = express();
 app.use(cors({}));
-const upload = multer({ dest: 'uploads/' }); // Temporary upload directory
+ // Temporary upload directory
 const PORT = process.env.PORT || 5000;
+
+dotenv.config()
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(fileUpload({
+  limits: { fileSize: 50 * 1024 * 1024 }, // Set limit to 50MB
+}));
+
+app.use("/api/user", publicUser)
+app.use("/api/user", auth, privateUser)
+app.use("/api/public/book", publicBook)
+app.use("/api/book", auth, privateBook)
+app.use("/api", root);
+app.use("/", root);
+
+const upload = multer({ dest: 'uploads/' });
 
 // Endpoint to upload chunks
 app.post('/api/book/upload', upload.single('fileChunk'), (req, res) => {
@@ -109,30 +125,6 @@ app.post('/api/book/finalize', express.json(), async (req, res) => {
   }
 });
 
-
-
-
-dotenv.config()
-
-
-
-
-
-
-
-
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true}));
-app.use(fileUpload({
-  limits: { fileSize: 50 * 1024 * 1024 }, // Set limit to 50MB
-}));
-
-app.use("/api/user", publicUser)
-app.use("/api/user", auth, privateUser)
-app.use("/api/public/book", publicBook)
-app.use("/api/book", auth, privateBook)
-app.use("/api", root);
-app.use("/", root);
 
 
 async function startApp() {
